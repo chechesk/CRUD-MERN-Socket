280066ec5a2cbf814a27cbdbf814a221b43cca8b
Jose Gregorio Romero Mendoza <jgromerom89@gmail.com>
2024-11-23 23:55:13 -0300

    update
    test and add password

--- backend/src/Database/Modal/userSchema.js
+++ backend/src/Database/Modal/userSchema.js
@@ -1,12 +1,41 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt')
+
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
   name: { type: String, required: true },
   lastname: { type: String, required: true },
   email: { type: String, required: true, unique: true },
+  password: { type: String, required: true }, // Contraseña encriptada
   birthdate: { type: Date, required: true },
   creation_date: { type: Date, default: Date.now },
 });
+
+// Hook para encriptar la contraseña antes de guardar
+userSchema.pre('save', async function (next) {
+  const user = this;
+
+  // Si la contraseña no ha sido modificada, salta este paso
+  if (!user.isModified('password')) return next();
+
+  try {
+    // Genera el salt y encripta la contraseña
+    const salt = await bcrypt.genSalt(10); // 10 es el factor de costo recomendado
+    user.password = await bcrypt.hash(user.password, salt);
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Método para comparar contraseñas
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  try {
+    return await bcrypt.compare(candidatePassword, this.password);
+  } catch (err) {
+    throw new Error(err);
+  }
+};
 
 module.exports = mongoose.model('Users', userSchema);
--- backend/src/__tests__/userModel.test.js
+++ backend/src/__tests__/userModel.test.js
@@ -1,26 +1,27 @@
 const mongoose = require('mongoose');
 const { MongoMemoryServer } = require('mongodb-memory-server');
 const User = require('../Database/Modal/userSchema');
+const bcrypt = require('bcrypt')
 
 let mongoServer;
 
 beforeAll(async () => {
-  // Inicia MongoMemoryServer
+  
   mongoServer = await MongoMemoryServer.create();
   const uri = mongoServer.getUri();
 
-  // Conéctate a la base de datos en memoria
+  
   await mongoose.connect(uri);
 });
 
 afterAll(async () => {
-  // Cierra la conexión y detén MongoMemoryServer
+  
   await mongoose.disconnect();
   await mongoServer.stop();
 });
 
 afterEach(async () => {
-  // Limpia la colección de usuarios después de cada prueba
+  
   await User.deleteMany();
 });
 
@@ -30,24 +31,33 @@ describe('User Model Tests', () => {
       name: 'John',
       lastname: 'Doe',
       email: 'john.doe@example.com',
+      password: '123',
       birthdate: new Date('1990-01-01'),
     };
 
-    const validUser = new User(userData);
-    const savedUser = await validUser.save();
+    const savedUser = await User.create(userData);
 
-    expect(savedUser._id).toBeDefined();
+    // Validar los datos del usuario
     expect(savedUser.name).toBe(userData.name);
     expect(savedUser.lastname).toBe(userData.lastname);
     expect(savedUser.email).toBe(userData.email);
     expect(savedUser.birthdate).toEqual(userData.birthdate);
+
+    // Validar que la contraseña fue encriptada
+    expect(savedUser.password).not.toBe(userData.password);
+    expect(savedUser.password).toMatch(/^\$2b\$10\$/);
+
+    // Verificar que la contraseña original coincide con la encriptada
+    const isPasswordValid = await bcrypt.compare(userData.password, savedUser.password);
+    expect(isPasswordValid).toBe(true);
   });
 
   it('Should fail if a required field is missing', async () => {
     const userData = {
       lastname: 'Doe',
       email: 'john.doe@example.com',
-      birthdate: new Date('1990-01-01'),
+      password:  '1234',
+      birthdate: new Date('1990-01-01')
     };
 
     try {
@@ -64,6 +74,7 @@ describe('User Model Tests', () => {
       name: 'John',
       lastname: 'Doe',
       email: 'john.doe@example.com',
+      password: '1234',
       birthdate: new Date('1990-01-01'),
     };
   
